dfecb3967555557e896480a44d46fd8e0f2ad767
Ivan Ivanov <Ivan.Ivanov@kordia.co.nz>
2017-10-18 09:17:17 +1300

    Save trips when edit

--- server/trips/views.py
+++ server/trips/views.py
@@ -1,3 +1,6 @@
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -31,20 +34,17 @@ class TripList(APIView):
 class TripDetail(APIView):
 
     def get(self, request, id):
-        trips = Trip.objects.filter(id=id).first()
-        # TODO: check user permissions
-
-
-
-        serializer = TripS(trips, many=True)
+        trip = get_object_or_404(Trip, id=id)
+        if trip.user != request.user and request.user.profile.role != UserProfile.ADMIN:
+            raise Http404  # we don't want them to find valid ids by brute force
+        serializer = TripS(trip)
         return Response({'trips': serializer.data})
 
-    def post(self, request):
-        form = TripForm(request.data.get('trip'))
+    def put(self, request, id):
+        trip = get_object_or_404(Trip, id=id)
+        form = TripForm(request.data.get('trip'), instance=trip)
         if not form.is_valid():
             return Response({'errors': form.errors}, status=status.HTTP_400_BAD_REQUEST)
-        new_trip = form.save(commit=False)
-        new_trip.user = request.user
-        new_trip.save()
+        new_trip = form.save()
         serializer = TripS(new_trip)
-        return Response({'trip': serializer.data}, status=status.HTTP_201_CREATED)
+        return Response({'trip': serializer.data})
